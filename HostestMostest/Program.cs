db2f10f94726a459526c023bff7ed87a4bc0b085
Kevin Bost <kitokeboo@gmail.com>
2023-07-10 21:06:06 -0700

    Default setup

---
+++ HostestMostest/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Hosting;
+
+IHostBuilder hostBuilder = new HostBuilder();
+using IHost host = hostBuilder.Build();
+
+Console.WriteLine("Starting app...");
+await host.StartAsync();
+
+Console.WriteLine("Running app...");
+await host.StopAsync();
+
+Console.WriteLine("Waiting for shutdown...");
+await host.WaitForShutdownAsync();
+
+Console.WriteLine("Shutdown...");
